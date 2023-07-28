a403f79cb9969d0a276b823a3b4474297ecc3f5e
Olga <49266934+olgakos@users.noreply.github.com>
2023-07-28 11:03:58 +0300

    Update and rename archivberlin_test.py to berlin2_test.py

--- tests/berlin2_test.py
+++ tests/berlin2_test.py
@@ -3,27 +3,19 @@ from allure_commons.types import Severity
 from selene import browser, be, by, have
 from selene.support.shared import browser
 from selene.support.conditions import be
+import pytest
+from allure_commons._allure import step
 
-from tests.conftest import URL_LINK_ALLURE
-
-URL_LINK_ALLURE = 'https://landesarchiv-berlin.de'
-
-# Строка sys.path.append добавляет родительский каталог каталога тестов в путь поиска модулей Python, 
-# что позволяет импортировать модули из корневой папки.
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(file)))
-                
 @allure.tag('web')
 @allure.severity(Severity.NORMAL)
 @allure.label('owner', 'Olga')
 @allure.feature(f'Проверка1')
 @allure.story('Стори1')
-@allure.link(URL_LINK_ALLURE, name="Link to Home Page")
-def test1rem(setup_browser):
-
+@allure.link('https://landesarchiv-berlin.de', name="Link to Home Page")
+def test1rem():
     value = 'Das Landesarchiv Berlin'
-
     with allure.step("Open home page"):
-        browser.open('/')
+        browser.open('http://landesarchiv-berlin.de')
     with allure.step("Кнопка лупа"):
         browser.element('.search-toggle').click()
     with allure.step("В поле ввода пусто"):
@@ -35,13 +27,11 @@ def test1rem(setup_browser):
 @allure.label('owner', 'Olga')
 @allure.feature(f'Проверка1')
 @allure.story('Стори1')
-@allure.link(URL_LINK_ALLURE, name="Link to Home Page")
+@allure.link('https://landesarchiv-berlin.de', name="Link to Home Page")
 def test2loc():
-
     value = 'Das Landesarchiv Berlin'
-
     with allure.step("Open home page2"):
-        browser.open('/')
+        browser.open('http://landesarchiv-berlin.de')
     with allure.step("Кнопка лупа2"):
         browser.element('.search-toggle').click()
     with allure.step("В поле ввода пусто2"):
