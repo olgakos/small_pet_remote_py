3117427f2a3c98296caaea249a1690cb709c0a21
Olga <49266934+olgakos@users.noreply.github.com>
2023-07-28 17:00:05 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,9 +22,14 @@ def setup_browser():
             "enableVideo": True
         }
     }
+    
+    options.add_experimental_option("prefs", {"intl.accept_languages": "de,de-DE"}) #!
+    
     options.capabilities.update(selenoid_capabilities)
+    
     login = os.getenv('LOGIN')
     password = os.getenv('PASSWORD')
+    
     driver = webdriver.Remote(
         command_executor=f'https://{login}:{password}@selenoid.autotests.cloud/wd/hub',
         options=options)
@@ -39,8 +44,8 @@ def setup_browser():
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_video(browser)
-    
-    browser.quit()
+    browser.quit() 
+
 
 
 #---------LOCAL------
@@ -51,9 +56,16 @@ def browser_management():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     driver_options = webdriver.ChromeOptions()
-    #driver_options.add_argument('--headless')
+    # driver_options.add_argument('--headless') #!
     browser.config.driver_options = driver_options
-
+    
+    driver_options.add_experimental_option("prefs", {"intl.accept_languages": "de,de-DE"}) #!
+    # options.add_experimental_option("prefs", {"intl.accept_languages": "en,en_US"})
+    # options.add_experimental_option("prefs", {"intl.accept_languages": "de,de-DE"})
+    # chrome_options.add_argument("--lang=en")
+    #         OR
+    # chrome_options.add_argument("--lang=en-US")    
+   
     yield
 
     browser.quit()
